f844ee0740a34b46074b4357e3851af5c739dbc8
Muhammad Hasbi Ar-Rasid <muhammadhasbiarrasid@gmail.com>
2023-05-26 17:57:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10120228_muhammadhasbiarrasid/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.utspraktik_if6_10120228_muhammadhasbiarrasid
+
+//Nama    : Muhammad Hasbi Ar-Rasid
+//NIM     : 10120228
+//Kelas   : IF 6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
